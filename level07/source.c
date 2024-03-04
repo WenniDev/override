be97f4da9e99ce40635a1f5f8632243cd712ecc7
rertzer <rertzer@student.42.fr>
2024-03-04 14:43:30 +0100

    level07 source clean

--- level07/source.c
+++ level07/source.c
@@ -1,60 +1,60 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 void clear_stdin(void)
 {
-  char c;
-  
-  do {
+  char c = 0;
+
+  while (c != '\n' && c != 255)
+  {
     c = getchar();
-    if (c == '\n') {
-      return;
-    }
-  } while (c != -1);
+  }
   return;
 }
 
 unsigned int get_unum(void)
 {
   unsigned int unsigned_number;
-  
+
   unsigned_number = 0;
   fflush(stdout);
-  scanf("%u",unsigned_number);
+  scanf("%u", unsigned_number);
   clear_stdin();
   return unsigned_number;
 }
 
-int store_number(int offset)
+int store_number(char *storage)
 {
   unsigned int number;
   unsigned int index;
-  int return_value;
-  
+  int error;
+
   printf(" Number: ");
   number = get_unum();
   printf(" Index: ");
   index = get_unum();
-  if ((index % 3 == 0) || (number >> 0x18 == 0xb7))
+  if ((index % 3 == 0) || (number >> 24 == 183))
   {
     puts(" *** ERROR! ***");
     puts("   This index is reserved for wil!");
     puts(" *** ERROR! ***");
-    return_value = 1;
+    error = 1;
   }
-  else {
-    *(unsigned int *)(index * 4 + offset) = number;
-    return_value = 0;
+  else
+  {
+    *(unsigned int *)(index * 4 + storage) = number;
+    error = 0;
   }
-  return return_value;
+  return error;
 }
 
-int read_number(int number)
+int read_number(char *storage)
 {
   unsigned int index;
-  
+
   printf(" Index: ");
-  unsigned int index = get_unum();
-  printf(" Number at data[%u] is %u\n", index, *(unsigned int *)(index * 4 + number));
+  index = get_unum();
+  printf(" Number at data[%u] is %u\n", index, *(unsigned int *)(storage + index * 4));
   return 0;
 }
 
@@ -62,154 +62,53 @@ int read_number(int number)
 
 int main(int argc, char **argv, char **envp)
 {
-  char cVar1;
-  int iVar2; // probably a temporary value used for different purposes
-  uint uVar3;
-  int    *perhaps_memory_storage_pointer;   //pointer to int or unsigned int
-  char *pcVar5;
-  byte *pbVar6;
-  int in_GS_OFFSET;
-  bool bVar7;
-  bool bVar8;
-  bool bVar9;
-  byte bVar10;
-  char **local_1c8;
-  
-  undefined4 local_1bc [100];
-  undefined4 local_2c;
-  undefined4 local_28;
-  undefined4 local_24;
-  undefined4 local_20;
-  undefined4 local_1c;
-  undefined4 local_18;
-  int local_14;
-  
-  bVar10 = 0;
-  char **arguments = argv;
-  local_1c8 = envp;
-  local_14 = *(int *)(in_GS_OFFSET + 0x14);
-  local_2c = 0;
-  local_28 = 0;
-  local_24 = 0;
-  local_20 = 0;
-  local_1c = 0;
-  local_18 = 0;
-  perhaps_memory_storage_pointer = local_1bc;
+  char command[20] = {0};
+  int error = 0;
+  char storage[100];
 
+  memset(command, 0, 100);
 
-  for (int i = 100; i != 0; --i)
+  for (; *argv != '\0'; ++argv)
   {
-    *perhaps_memory_storage_pointer = 0;
-    ++perhaps_memory_storage_pointer;
+    memset(*argv, 0, strlen(argv));
   }
-  for (; *arguments != '\0'; ++arguments)
+  for (; *envp != '\0'; ++envp)
   {
-    uVar3 = -1;
-    pcVar5 = *arguments;
-    do {
-      if (uVar3 == 0) break;
-      uVar3 = uVar3 - 1;
-      cVar1 = *pcVar5;
-      pcVar5 = pcVar5 + (uint)bVar10 * -2 + 1;
-    } while (cVar1 != '\0');
-    memset(*arguments,0,~uVar3 - 1);
-  }
-  for (; *local_1c8 != (char *)0x0; local_1c8 = local_1c8 + 1) {
-    uVar3 = 0xffffffff;
-    pcVar5 = *local_1c8;
-    do {
-      if (uVar3 == 0) break;
-      uVar3 = uVar3 - 1;
-      cVar1 = *pcVar5;
-      pcVar5 = pcVar5 + (uint)bVar10 * -2 + 1;
-    } while (cVar1 != '\0');
-    memset(*local_1c8,0,~uVar3 - 1);
+    memset(*envp, 0, strlen(envp));
   }
   puts(
-      "----------------------------------------------------\n  Welcome to wil\'s crappy number storage service!   \n----------------------------------------------------\n Commands:                                          \n    store - store a number into the data storage    \n    read  - read a number from the data storage     \n    quit  - exit the program                        \n----------------------------------------------------\n   wil has reserved some storage :>                 \n----------------------------------------------------\n"
-      );
-  do {
+      "----------------------------------------------------\n  Welcome to wil\'s crappy number storage service!   \n----------------------------------------------------\n Commands:                                          \n    store - store a number into the data storage    \n    read  - read a number from the data storage     \n    quit  - exit the program                        \n----------------------------------------------------\n   wil has reserved some storage :>                 \n----------------------------------------------------\n");
+  while (true)
+  {
     printf("Input command: ");
-    local_2c = 1;
-    fgets((char *)&local_28,0x14,stdin);
-    uVar3 = 0xffffffff;
-    perhaps_memory_storage_pointer = &local_28;
-    do {
-      if (uVar3 == 0) break;
-      uVar3 = uVar3 - 1;
-      cVar1 = *(char *)perhaps_memory_storage_pointer;
-      perhaps_memory_storage_pointer = (undefined4 *)((int)perhaps_memory_storage_pointer + (uint)bVar10 * -2 + 1);
-    } while (cVar1 != '\0');
-    uVar3 = ~uVar3;
-    bVar7 = uVar3 == 1;
-    bVar9 = uVar3 == 2;
-    *(undefined *)((int)&local_2c + uVar3 + 2) = 0;
-    iVar2 = 5;
-    perhaps_memory_storage_pointer = &local_28;
-    pbVar6 = (byte *)"store";
-    do {
-      if (iVar2 == 0) break;
-      iVar2 = iVar2 + -1;
-      bVar7 = *(byte *)perhaps_memory_storage_pointer < *pbVar6;
-      bVar9 = *(byte *)perhaps_memory_storage_pointer == *pbVar6;
-      perhaps_memory_storage_pointer = (undefined4 *)((int)perhaps_memory_storage_pointer + (uint)bVar10 * -2 + 1);
-      pbVar6 = pbVar6 + (uint)bVar10 * -2 + 1;
-    } while (bVar9);
-    bVar8 = false;
-    bVar7 = (!bVar7 && !bVar9) == bVar7;
-    if (bVar7) {
-      local_2c = store_number(local_1bc);
+    error = 1;
+    fgets(command, 20, stdin);
+
+    if (strncmp(command, "store", 5) == 0)
+    {
+      error = store_number(storage);
     }
-    else {
-      iVar2 = 4;
-      perhaps_memory_storage_pointer = &local_28;
-      pbVar6 = &DAT_08048d61;
-      do {
-        if (iVar2 == 0) break;
-        iVar2 = iVar2 + -1;
-        bVar8 = *(byte *)perhaps_memory_storage_pointer < *pbVar6;
-        bVar7 = *(byte *)perhaps_memory_storage_pointer == *pbVar6;
-        perhaps_memory_storage_pointer = (undefined4 *)((int)perhaps_memory_storage_pointer + (uint)bVar10 * -2 + 1);
-        pbVar6 = pbVar6 + (uint)bVar10 * -2 + 1;
-      } while (bVar7);
-      bVar9 = false;
-      bVar7 = (!bVar8 && !bVar7) == bVar8;
-      if (bVar7) {
-        local_2c = read_number(local_1bc);
-      }
-      else {
-        iVar2 = 4;
-        perhaps_memory_storage_pointer = &local_28;
-        pbVar6 = &DAT_08048d66;
-        do {
-          if (iVar2 == 0) break;
-          iVar2 = iVar2 + -1;
-          bVar9 = *(byte *)perhaps_memory_storage_pointer < *pbVar6;
-          bVar7 = *(byte *)perhaps_memory_storage_pointer == *pbVar6;
-          perhaps_memory_storage_pointer = (undefined4 *)((int)perhaps_memory_storage_pointer + (uint)bVar10 * -2 + 1);
-          pbVar6 = pbVar6 + (uint)bVar10 * -2 + 1;
-        } while (bVar7);
-        if ((!bVar9 && !bVar7) == bVar9) {
-          if (local_14 == *(int *)(in_GS_OFFSET + 0x14)) {
-            return 0;
-          }
-                    // WARNING: Subroutine does not return
-          __stack_chk_fail();
-        }
-      }
+    else if (strncmp(command, "read", 4) == 0)
+    {
+      error = read_number(storage);
     }
-    if (local_2c == 0) {
-      printf(" Completed %s command successfully\n",&local_28);
+    else if (strncmp(command, "quit", 4) == 0)
+    {
+      return 0;
     }
-    else {
-      printf(" Failed to do %s command\n",&local_28);
+
+    if (error == 0)
+    {
+      printf(" Completed %s command successfully\n", command);
     }
-    local_28 = 0;
-    local_24 = 0;
-    local_20 = 0;
-    local_1c = 0;
-    local_18 = 0;
-  } while( true );
+    else
+    {
+      printf(" Failed to do %s command\n", command);
+    }
+    /*
+    ** it's probably not a memset in the real code;
+    ** the assembly do as if it is 5 int set to 0;
+    */
+    memset(command, 0, 20);
+  };
 }
-
-
